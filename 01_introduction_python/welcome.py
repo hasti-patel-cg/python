5021562441432e9be55d3e28cf4ea5106137002d
Hasti Patel <hasti.patel.cg@gmail.com>
2026-08-30 20:59:38 +0530

    add Arithmatic oprection page

--- 01_introduction_python/welcome.py
+++ 01_introduction_python/welcome.py
@@ -1,1 +1,11 @@
-# Welcome to Python!     
+# Welcome to Python!  
+print("Welcome to Python!")
+age=17
+name="hasti"
+marks=90.0
+attendance=True
+result=None
+print(type(name), type(age), type(marks), type(attendance), type(result))
+
+
+
